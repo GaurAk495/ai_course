2eaee0b5fcdb1763cd67221dc3f4a13705841314
Ak <gaurak495@gmail.com>
2026-01-09 01:58:10 +0530

    orm:Implement singleton pattern for Next.js and improve database pool management.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,27 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../prisma_client/client";
+import { Pool } from "pg";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL;
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+if (!connectionString) {
+  throw new Error("Please provide a DATABASE_URL in the environment variables");
+}
 
-export { prisma };
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+    log: ["error", "warn"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
